7f3f585c4c9a4dce47b996fe8d8745805379cb8b
NitinAgrawalgit <83645245+NitinAgrawalgit@users.noreply.github.com>
2021-05-05 12:27:32 +0530

    Add files via upload

---
+++ Using Comments.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>  //Preprocessor directive to link the header file stdio.h for standard input/output functions. 
+// This program was developed by Nitin Agrawal.
+main() 
+{//Opening brace of the main function
+	printf("This program demonstrates the use of comments in C language. \n"); 
+	/* I had no idea what to write
+	here so I just wrote this to demonstrate the use of multi line comments.*/
+	printf("Comments in C can be: \n1. Single line comments \n2. Multi line comments\n\n");
+	printf("Note:- Comments in C can't be nested.");
+}//Closing brace of the main function
