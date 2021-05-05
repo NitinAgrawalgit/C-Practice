4de29387f73e950df5c5c358213c36a8336de96f
NitinAgrawalgit <83645245+NitinAgrawalgit@users.noreply.github.com>
2021-05-05 12:28:20 +0530

    Add files via upload

---
+++ Printing multiple variables in single line.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main()
+{
+	int states, un_ter;
+	float population;
+	
+	states = 28;
+	un_ter = 8;
+	population = 1.395;
+	
+	printf("The country of India has %d states and %d Union territories with a total population of %4.3f billion.\n", states, un_ter, population);
+}
