8d3d58aa8afdc03502aba20ea59fe27e1e6afd89
NitinAgrawalgit <83645245+NitinAgrawalgit@users.noreply.github.com>
2021-05-05 12:27:55 +0530

    Add files via upload

---
+++ Using a Variable.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// This program was dveloped by Nitin Agrawal.
+// Date: 5th May, 2021
+main()
+{
+	int int_var;
+	char char_var;
+	int_var = 10;
+	char_var = 'g';
+	
+	printf("Value of the first variable = %d \n", int_var);
+	printf("Value of the second variable = %c \n\n", char_var);
+	printf("Decimal value of the second variable = %d", char_var); //ASCII Value
+}
